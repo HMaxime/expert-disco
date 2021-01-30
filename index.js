3f11c9ebb910cb759305bad96f6f6017a7bf6464
Bill Boquet <mag.hamon22@gmail.com>
2021-01-30 11:30:54 +0100

    random music, youtube link

---
+++ index.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { YTSearcher } = require('ytsearcher');
+const searcher = new YTSearcher(process.env.API_KEY);
+
+const providers = ["YouTube", "Spotify"]
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const Music = (e) => {
+    return {
+        "title": e.title,
+        "description": e.description,
+        "provider": e.provider.name,
+        "url": e.url
+    }
+}
+
+client.on('message', async msg => {
+    if (msg.channel.id === process.env.ID_CALL_CHANNEL) {
+        const args = msg.content.split(" ")
+        if (args.includes("-rm") || args.includes("-randomMusic")) {
+
+            const musics = (await (await client.channels.fetch(process.env.ID_SEARCH_CHANNEL)).messages.fetch())
+                .filter(msg => msg.embeds
+                    .map(e => providers.includes(e.provider?.name))
+                    .includes(true))
+                .map(msg => msg.embeds.map(e => Music(e)))
+                .flat()
+
+            if(!musics){
+                msg.reply("Music not found")
+                return
+            }
+            console.log(`Music found : ${musics.length}`)
+            const randomMusic = musics[Math.floor(Math.random() * musics.length)]
+
+            if (randomMusic?.provider === "Spotify") {
+                const ytUrl = (await searcher.search(randomMusic.title + randomMusic.description, { type: "video", maxResults: 1 }))?.first?.url
+                if (ytUrl) randomMusic.url = ytUrl
+            }
+            
+            msg.reply(randomMusic?.url || "Music not found")
+        }
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
