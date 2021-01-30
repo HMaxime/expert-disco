2f1495e735b788a2fd601a6d7d718b04fd443c5a
Bill Boquet <mag.hamon22@gmail.com>
2021-01-30 12:14:50 +0100

    set activity to watching

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const providers = ["YouTube", "Spotify"]
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
 });
 
 const Music = (e) => {
