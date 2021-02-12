7e1f1d104f5413433a3bd95bacaa9a23ce5306da
Bill Boquet <mag.hamon22@gmail.com>
2021-02-12 21:13:11 +0100

    - YouTube search - Adding in real time - Search random musics

--- index.js
+++ index.js
@@ -1,53 +1,102 @@
 require('dotenv').config();
 
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const discordClient = new Discord.Client();
+
+const { MongoClient } = require("mongodb");
+const { resolve } = require('mongodb/lib/core/topologies/read_preference');
+
 const { YTSearcher } = require('ytsearcher');
-const searcher = new YTSearcher(process.env.API_KEY);
+const searcher = new YTSearcher(process.env.YOUTUBE_API_KEY);
 
 const providers = ["YouTube", "Spotify"]
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
-});
+const dbUrl = `mongodb+srv://${process.env.DB_CLIENT_NAME}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_NAME}/${process.env.DB_NAME}?retryWrites=true&w=majority`
+const mongoClient = new MongoClient(dbUrl, { useUnifiedTopology: true });
 
-const Music = (e) => {
-    return {
-        "title": e.title,
-        "description": e.description,
-        "provider": e.provider.name,
-        "url": e.url
+const onlyUnique = (value, index, self) => {
+    return self.indexOf(value) === index;
+}
+
+const getYoutubeLink = async msg => {
+    const embed = msg.embeds[0]
+    if (embed?.provider?.name === "Spotify") {
+        const url = (await searcher.search(embed.title + embed.description, { type: "video", maxResults: 1 }))?.first?.url
+        //const url = new Promise(resolve => setTimeout(() => resolve("truc"), 100))
+
+        return url || embed.url
+    } else {
+        return embed.url
     }
 }
 
-client.on('message', async msg => {
-    if (msg.channel.id === process.env.ID_CALL_CHANNEL) {
-        const args = msg.content.split(" ")
-        if (args.includes("-rm") || args.includes("-randomMusic")) {
+mongoClient.connect().then(() => {
+    const database = mongoClient.db(process.env.DB_NAME)
+    const collection = database.collection(process.env.DB_NAME)
 
-            const musics = (await (await client.channels.fetch(process.env.ID_SEARCH_CHANNEL)).messages.fetch())
-                .filter(msg => msg.embeds
-                    .map(e => providers.includes(e.provider?.name))
-                    .includes(true))
-                .map(msg => msg.embeds.map(e => Music(e)))
-                .flat()
+    discordClient.on('ready', async () => {
+        console.log(`Logged in as ${discordClient.user.tag}!`);
+        discordClient.user.setActivity('chat', { type: 'PLAYING' });
+        const limit = 100
+        let messages = []
+        let before = undefined
+        let links = []
+        try {
+            console.log("Starting indexation...")
+            do {
+                messages = await (await discordClient.channels.fetch(process.env.ID_SEARCH_CHANNEL)).messages.fetch({ limit: limit, before: before })
+                links = (await Promise.all(getDatasFromMessages(messages)
+                    .map(d => d.musicUrl)))
+                    .filter(onlyUnique)
+                try {
+                    await collection.insertMany(links.map(l => { return { _id: l } }), { ordered: false });
+                    links.forEach(l => console.log(`${l} Added`))
+                } catch (e) { }
 
-            if(!musics){
-                msg.reply("Music not found")
-                return
-            }
-            console.log(`Music found : ${musics.length}`)
-            const randomMusic = musics[Math.floor(Math.random() * musics.length)]
-
-            if (randomMusic?.provider === "Spotify") {
-                const ytUrl = (await searcher.search(randomMusic.title + randomMusic.description, { type: "video", maxResults: 1 }))?.first?.url
-                if (ytUrl) randomMusic.url = ytUrl
-            }
-            
-            msg.reply(randomMusic?.url || "Music not found")
+                before = messages?.last()?.id
+            } while (messages.size == limit)
+        } catch (error) {
+            console.error(error)
+        } finally {
+            console.log("\nIndexation finished")
         }
-    }
-});
+    });
 
-client.login(process.env.BOT_TOKEN);
+    const getDatasFromMessages = (messages) => {
+        return messages.filter(msg => {
+            return msg.embeds
+                .map(e => providers.includes(e.provider?.name))
+                .includes(true)
+        }).map(msg => {
+            return {
+                idMessage: msg.id,
+                musicUrl: getYoutubeLink(msg),
+            }
+        })
+    }
+
+    discordClient.on('message', async msg => {
+        if (msg.channel.id === process.env.ID_CALL_CHANNEL) {
+            const args = msg.content.split(" ")
+            if (args.includes("-rm") || args.includes("-randomMusic")) {
+                // get random music from database
+                try {
+                    await collection.aggregate([{ $sample: { size: 1 } }])
+                        .toArray()
+                        .then(d => {
+                            msg.reply(d[0]._id)
+                        })
+                } catch (error) {
+                    console.error(error)
+                }
+            }
+        } else if (msg.channel.id === process.env.ID_SEARCH_CHANNEL && msg.embeds.map(e => providers.includes(e.provider?.name)).includes(true)) {
+            // Add music to the database if it is a music
+            const link = await getYoutubeLink(msg)
+            console.log(`${link} Added`)
+            await collection.replaceOne({ "_id": link }, { _id: link }, { upsert: true })
+        }
+    });
+
+    discordClient.login(process.env.BOT_TOKEN);
+})
